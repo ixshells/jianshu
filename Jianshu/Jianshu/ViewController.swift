21fbe804fa0223a4534771e9c4e09d9e8a07d3d2
ixshells <ixshells@163.com>
2018-03-17 14:17:23 +0800

    initial project

---
+++ Jianshu/Jianshu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jianshu
+//
+//  Created by Tao Xue on 14/03/2018.
+//  Copyright © 2018 Tao Xue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
